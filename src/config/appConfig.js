7fa1f0b46ac453cd622d14a62bf1e26e9f7514b9
Jose Pita <joseacureropita0@gmail.com>
2026-03-20 03:00:31 +0100

    remove default messages

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,7 +1,7 @@
-const DEFAULT_SYSTEM_NAME = 'GIRASOL_PA_LA_GIGI_OS_V1.0';
-const DEFAULT_BROWSER_TITLE = 'GIGI';
+const DEFAULT_SYSTEM_NAME = 'GIRASOL_OS_V1.0';
+const DEFAULT_BROWSER_TITLE = 'GIRASOL';
 const DEFAULT_INTRO_MESSAGE =
-  'Te amo chisqui, no te pude mandar flores, pero te dejo esta que nunca se te va a marchitar, y siempre vas a poder tener. Espero poco a poco ir llegando a ti';
+  '';
 
 export const APP_CONFIG = {
   systemName: import.meta.env.VITE_SYSTEM_NAME || DEFAULT_SYSTEM_NAME,
